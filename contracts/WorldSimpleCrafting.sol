b152c36c5ff21a57b69677c7c3719d487982f59a
Mouse Dev <mousedev2@gmail.com>
2023-01-30 13:01:27 -0500

    Added craft functionality to crafting

--- contracts/WorldSimpleCrafting.sol
+++ contracts/WorldSimpleCrafting.sol
@@ -2,69 +2,120 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "./libraries/WorldStateStorage.sol";
 import "./interfaces/IERC20Consumer.sol";
 import "hardhat/console.sol";
 
-
 enum TOKENTYPE {
     ERC20,
     ERC721,
-    ERC1155,
-    
+    ERC1155
 }
-
-struct FunctionalResult{
-    address target;
-    bytes4 signature;
-    bytes callData;
-}
-
 
 struct Ingredient {
     address tokenAddress;
     TOKENTYPE tokenType;
-
     //One or both of these will be used depending on the tokentype.
     uint256 tokenId;
     uint256 tokenQuantity;
 }
 
 struct Result {
-    FunctionalResult functionalResult;
+    address target;
+    bytes4 selector;
+    bytes params;
 }
 
-
-struct CraftingRecipe{
+struct CraftingRecipe {
     //Store array of essential ingredients
     Ingredient[] ingredients;
-
     //Store array of outputs
     Result[] results;
-
     //Store whether this has been anointed. Defaults to false.
     bool anointed;
-
     //Store when this was anointed, must have been more than x time ago.
     uint256 anointmentTime;
 }
 
-
 contract WorldSimpleCrafting {
-
     mapping(uint256 => CraftingRecipe) internal craftingRecipes;
 
     uint256 _currentRecipeId = 1;
-
-
 
     function createNewRecipe(CraftingRecipe memory _craftingRecipe) public {
         craftingRecipes[_currentRecipeId] = _craftingRecipe;
         _currentRecipeId++;
     }
 
-    function getRecipe(uint256 _recipeId) public view returns(CraftingRecipe memory) {
+    function getRecipe(uint256 _recipeId)
+        public
+        view
+        returns (CraftingRecipe memory)
+    {
         return craftingRecipes[_recipeId];
     }
 
+    function craft(uint256 _recipeId) public {
+        CraftingRecipe memory _craftingRecipe = craftingRecipes[_recipeId];
+
+        //10 minutes
+        //TODO
+        //Adjust anointment timelock method.
+        require(block.timestamp >= _craftingRecipe.anointmentTime + 600);
+
+        for (uint256 i = 0; i < _craftingRecipe.ingredients.length; i++) {
+            //Pull all the ingredients
+            Ingredient memory _ingredient = _craftingRecipe.ingredients[i];
+
+            if (_ingredient.tokenType == TOKENTYPE.ERC20) {
+                //ERC20
+                IERC20(_ingredient.tokenAddress).transferFrom(
+                    msg.sender,
+                    address(this),
+                    _ingredient.tokenQuantity
+                );
+            }
+
+            if (_ingredient.tokenType == TOKENTYPE.ERC721) {
+                //ERC721
+                IERC721(_ingredient.tokenAddress).transferFrom(
+                    msg.sender,
+                    address(this),
+                    _ingredient.tokenId
+                );
+            }
+
+            if (_ingredient.tokenType == TOKENTYPE.ERC1155) {
+                //ERC1155
+                IERC1155(_ingredient.tokenAddress).safeTransferFrom(
+                    msg.sender,
+                    address(this),
+                    _ingredient.tokenId,
+                    _ingredient.tokenQuantity,
+                    ""
+                );
+            }
+        }
+
+        for (uint256 i = 0; i < _craftingRecipe.results.length; i++) {
+            Result memory _result = _craftingRecipe.results[i];
+
+            (bool success, bytes memory data) = address(_result.target).call{
+                value: 0,
+                gas: 150000
+            }(abi.encodeWithSelector(_result.selector, _result.params));
+        }
+    }
+
+    function anoint(uint256 _recipeId) public /*onlyAdmin(_recipeId)*/ {
+        craftingRecipes[_recipeId].anointed = true;
+        craftingRecipes[_recipeId].anointmentTime = block.timestamp;
+    }
+
+    function unanoint(uint256 _recipeId)  public /*onlyAdmin(_recipeId)*/ {
+        craftingRecipes[_recipeId].anointed = false;
+        craftingRecipes[_recipeId].anointmentTime = 0;
+    }
 }
