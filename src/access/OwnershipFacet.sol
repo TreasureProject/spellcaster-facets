7c2cde1b16a795b2436e99c9fd7605847a1dfd2f
t1dev <t1dev@treasure.lol>
2023-10-24 14:48:53 -0700

    fix: eip173 was removed from oz; switch to eip5313

--- src/access/OwnershipFacet.sol
+++ src/access/OwnershipFacet.sol
@@ -2,10 +2,10 @@
 pragma solidity ^0.8.0;
 
 import { LibDiamond } from "../diamond/LibDiamond.sol";
-import { IERC173Upgradeable } from "@openzeppelin/contracts-diamond/interfaces/IERC173Upgradeable.sol";
+import { IERC5313Upgradeable } from "@openzeppelin/contracts-diamond/interfaces/IERC5313Upgradeable.sol";
 
-contract OwnershipFacet is IERC173Upgradeable {
-    function transferOwnership(address _newOwner) external override {
+contract OwnershipFacet is IERC5313Upgradeable {
+    function transferOwnership(address _newOwner) external {
         LibDiamond.enforceIsContractOwner();
         LibDiamond.setContractOwner(_newOwner);
     }
