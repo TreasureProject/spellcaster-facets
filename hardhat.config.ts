248da63cc6f5ea8ffebbdb1540a797c64aef225b
t1dev <t1dev@treasure.lol>
2023-11-27 12:05:48 -0700

    fix: missing paris evm version in compiler setting

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,34 +1,35 @@
 import "dotenv/config";
 import "@nomicfoundation/hardhat-foundry";
-import {HardhatUserConfig} from "hardhat/types";
+import { HardhatUserConfig } from "hardhat/types";
 
 const config: HardhatUserConfig = {
-  // These compiler settings must match foundry.toml to avoid unexpected issues
-  solidity: {
-    compilers: [
-      {
-        version: "0.8.22",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200
-          }
-        }
-      },
-    ],
-  },
-  defaultNetwork: "hardhat",
-  networks: {
-    hardhat: {
+    // These compiler settings must match foundry.toml to avoid unexpected issues
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.22",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200
+                    },
+                    evmVersion: "paris"
+                }
+            },
+        ],
     },
-    localhost: {
-      url: "http://localhost:8545",
-      chainId : 61337,
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+        },
+        localhost: {
+            url: "http://localhost:8545",
+            chainId: 61337,
+        },
     },
-  },
-  paths: {
-    sources: "src"
-  },
+    paths: {
+        sources: "src"
+    },
 };
 
 export default config;
