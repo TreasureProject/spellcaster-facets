46b444c24093adeeb1cd7bd28a18db2b8f607753
Antlion12 <antlion@treasure.lol>
2024-03-18 23:12:18 -0700

    feat: allow org admin to updateVault() with new owner and authoritySigner

--- test/OffchainAssetVaultManager.t.sol
+++ test/OffchainAssetVaultManager.t.sol
@@ -57,4 +57,58 @@ contract OffchainAssetVaultManagerTest is TestBase, DiamondManager {
         assertEq(address(leet), manager.getAuthoritySigner(org1, _vaultId));
         assertEq(address(leet), manager.getOwner(org1, _vaultId));
     }
+
+    function test_vault_update_success() public {
+        // Create the vault with initial vault parameters.
+        vm.expectEmit(true, true, true, false);
+        emit IOffchainAssetVaultManager.VaultCreated(org1, 1, address(this), address(this), address(this));
+        (address _vaultAddress, uint64 _vaultId) = manager.createVault(org1, address(this), address(this));
+        assertEq(_vaultId, 1);
+        assertEq(address(this), manager.getAuthoritySigner(org1, _vaultId));
+        assertEq(address(this), manager.getOwner(org1, _vaultId));
+
+        // Change the vault parameters.
+        vm.expectEmit(true, true, true, true);
+        emit IOffchainAssetVaultManager.VaultUpdated(org1, 1, _vaultAddress, address(leet), address(alice));
+        manager.updateVault(org1, 1, address(leet), address(alice));
+
+        // Ensure values have been changed.
+        assertEq(_vaultAddress, manager.getVaultAddress(org1, 1));
+        assertEq(address(leet), manager.getOwner(org1, 1));
+        assertEq(address(alice), manager.getAuthoritySigner(org1, 1));
+    }
+
+    function test_vault_update_fail() public {
+        // Create the vault with initial vault parameters.
+        vm.expectEmit(true, true, true, false);
+        emit IOffchainAssetVaultManager.VaultCreated(org1, 1, address(this), address(this), address(this));
+        (address _vaultAddress, uint64 _vaultId) = manager.createVault(org1, address(this), address(this));
+        assertEq(_vaultId, 1);
+        assertEq(address(this), manager.getAuthoritySigner(org1, _vaultId));
+        assertEq(address(this), manager.getOwner(org1, _vaultId));
+
+        // Expect invalid permissions to fail.
+        vm.prank(alice);
+        vm.expectRevert(
+            abi.encodeWithSelector(OrganizationManagerStorage.NotOrganizationAdmin.selector, address(alice))
+        );
+        manager.updateVault(org1, 1, address(leet), address(alice));
+
+        // Expect invalid vaultId to fail.
+        vm.expectRevert("Cannot modify invalid vaultId.");
+        manager.updateVault(org1, 0, address(leet), address(alice));
+        vm.expectRevert("Cannot modify invalid vaultId.");
+        manager.updateVault(org1, 2, address(leet), address(alice));
+
+        // Expect invalid owner to fail.
+        vm.expectRevert("Cannot set owner to null address.");
+        manager.updateVault(org1, 1, address(0), address(alice));
+        vm.expectRevert("Cannot set authoritySigner to null address.");
+        manager.updateVault(org1, 1, address(leet), address(0));
+
+        // Check vault info hasn't changed.
+        assertEq(_vaultAddress, manager.getVaultAddress(org1, 1));
+        assertEq(address(this), manager.getOwner(org1, 1));
+        assertEq(address(this), manager.getAuthoritySigner(org1, 1));
+    }
 }
